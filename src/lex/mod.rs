1963106e91bffc0926e08f3bbbe66863fc5af8ca
Dustella <fdnoaivj@outlook.com>
2022-12-04 21:07:05 +0800

    added switch mode

--- src/lex/mod.rs
+++ src/lex/mod.rs
@@ -2,7 +2,6 @@ mod inspector;
 use self::{gen_token::gen_token, table::ErrType};
 pub use crate::lex::inspector::*;
 use crate::lex::{dfa::dfa_transform, table::DfaState as state};
-use colored::*;
 // expose state to public
 pub use crate::lex::table::Token;
 
@@ -94,101 +93,9 @@ impl Tokenizer {
                 self.move_next();
                 return err;
             }
-
-            // println!("---------");
-            // dbg!(&last_state);
-            // dbg!(&buffer);
-            // dbg!(&self.current_state);
         }
         let res = gen_token(last_state, buffer);
         Ok(res.unwrap())
-    }
-}
-
-pub fn tokenize(pre_processed: Vec<String>) -> Vec<Result<Vec<Token>, (usize, String)>> {
-    let mut result_tokens = Vec::new();
-    for line in pre_processed {
-        let mut token_in_this_line: Vec<Token> = Vec::new();
-        let mut buffer = String::from("");
-        let mut current_state = state::Start;
-        let mut err_msg = String::new();
-        let mut has_err_this_line = 0;
-
-        let line = line.trim_start();
-
-        for (index, current_char) in line.chars().enumerate() {
-            let origin_state = current_state.clone();
-            dfa_transform(&mut current_state, current_char);
-
-            if let state::Start = current_state {
-                dfa_transform(&mut current_state, current_char);
-                let token = gen_token(origin_state, buffer.clone());
-                match token {
-                    None => {}
-                    Some(tok) => token_in_this_line.push(tok),
-                }
-                buffer.clear();
-            }
-
-            if let state::ErrFirst(ii) = current_state {
-                has_err_this_line = index + 1;
-
-                err_msg = match ii {
-                    table::ErrType::UnexpectedChar => "Unexpected char, found '".to_string(),
-                    table::ErrType::ExpectStringEnd => "Expected string end, found '".to_string(),
-                    table::ErrType::ExpectNumber => "Expected number, found '".to_string(),
-                };
-                if current_char == '\n' {
-                    err_msg.push_str("\\n");
-                } else {
-                    err_msg.push(current_char);
-                }
-                err_msg.push_str("'");
-                // break;
-            }
-            match (&current_state, current_char) {
-                (state::StringStartNow, ' ') => buffer.push(' '),
-                (_, ' ') => {}
-                (_, _) => buffer.push(current_char),
-            }
-        }
-        if has_err_this_line == 0 {
-            result_tokens.push(Ok(token_in_this_line));
-        } else {
-            result_tokens.push(Err((has_err_this_line, err_msg)))
-        }
-    }
-    result_tokens
-}
-
-pub fn show_tokens(tokens: &Vec<Result<Vec<Token>, (usize, String)>>, source: Vec<String>) {
-    for (index, line) in tokens.iter().enumerate() {
-        println!("line{}: {}", index + 1, source[index].trim());
-        match line {
-            Err((index, err_type)) => {
-                println!("Error:{}^", "-".repeat(*index));
-                println!(
-                    "{} {}",
-                    "Err at this line, postion".bright_red(),
-                    (*index).to_string().bright_red()
-                );
-                println!("{}", (*err_type).bright_red());
-            }
-            _ => {
-                for token in line.as_ref().unwrap() {
-                    match *token {
-                        Token::Boolean(_) => print!("{}, ", token.to_string().green()),
-                        Token::Identifier(_) => print!("{}, ", token.to_string().bright_white()),
-                        Token::Numbers(_) => print!("{}, ", token.to_string().purple()),
-                        Token::Reserved(_) => print!("{}, ", token.to_string().bright_yellow()),
-                        Token::Punctuator(_) => print!("{}, ", token.to_string().bright_blue()),
-                        Token::Litral(_) => print!("{}, ", token.to_string().purple()),
-                    }
-                }
-            }
-        }
-        println!();
-        println!();
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,22 @@ fn main() {
     // get file path from args
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
+    let mode = &args[2];
 
     // get content from file
     let content = fs::read_to_string(file_path).expect("err reading file");
     // get tokens
     let mut tokenizer = Tokenizer::new(content);
+    match mode.as_str() {
+        "lex" => {
+            lex::inspect(&mut tokenizer);
+        }
+        "parse" => {
+            parser::parse(&mut tokenizer).unwrap();
+        }
+        _ => {
+            println!("mode not found");
+        }
+    }
     // inspect(&mut tokenizer);
-
-    parser::parse(&mut tokenizer).unwrap();
 }
